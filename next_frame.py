f0440d3c9aa34810ae828a06906ed50ea201944e
Vlad Firoiu <vladfi2@gmail.com>
2016-08-16 20:06:49 -0400

    RNN model to predict next frames of games.

---
+++ next_frame.py
@@ -0,0 +1,69 @@
+import tensorflow as tf
+import tf_lib as tfl
+
+from vgdl.ontology import colors
+
+colorspace = len(colors)
+
+sequence_length = 50
+
+# TODO: batched inputs
+frames_nxy = tf.placeholder(tf.int64, [sequence_length, None, None])
+
+actions_n = tf.placeholder(tf.int64, [sequence_length])
+actions_bn = tf.expand_dums(actions_n, 0)
+
+image_dims = tf.shape(frames_nxy)[1:]
+image_width = image_dims[0]
+image_height = image_dims[1]
+
+frames_nxyc = tf.one_hot(frames_nxy, colorspace)
+
+# local info
+# TODO: make this hierarchical somehow, using "deconvolutions"?
+conv1 = tfl.convLayer(frames_nxyc, filter_size=5, filter_depth=32, pool_size=1)
+
+# global info
+summary = tf.reduce_max(conv1, [1, 2])
+summary = tf.expand_dims(summary, 0) # batch size 1 for now
+
+summary = tf.concat(1, [summary, actions_bn])
+
+gru = tf.nn.rnn_cell.GRUCell(64)
+outputs, _ = tf.nn.dynamic_rnn(gru, summary, dtype=tf.float32)
+#outputs, _ = tf.nn.rnn(gru, tf.unpack(summary), dtype=tf.float32)
+
+outputs = tf.squeeze(outputs, squeeze_dims=[0])
+
+# tile across width and height
+outputs = tf.expand_dims(outputs, 1)
+outputs = tf.expand_dims(outputs, 1)
+# need a recent tf for shape inference to get this
+outputs = tf.tile(outputs, tf.concat(0, [[1], image_dims, [1]]))
+
+# lots of intermediate reshaping here
+# which could be done only once, at the beginning
+features = tf.concat(3, [frames_nxyc, conv1, outputs])
+fc1 = tfl.affineLayer(features, 64, tf.nn.relu)
+logits = tfl.affineLayer(fc1, colorspace, bias=False)
+
+burn_in = 10 # allow the rnn to learn for a bit
+
+targets = frames_nxyc[1+burn_in:,:,:]
+logits = logits[burn_in:sequence_length-1,:,:]
+
+targets = tf.reshape(targets, [-1, colorspace])
+logits = tf.reshape(logits, [-1, colorspace])
+
+loss = tf.nn.softmax_cross_entropy_with_logits(logits, targets)
+loss = tf.reduce_mean(loss)
+
+update_op = tf.train.AdamOptimizer(1e-3).minimize(loss)
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+
+def train(frames, actions):
+  _, l = sess.run([], feed_dict = {frames_nxy:frames, actions_n:actions})
+  print(l)
+
